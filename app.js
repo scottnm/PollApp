5fbe4471e5c8cf747cbac919832631610387b063
Scott Munro <scottmunro@utexas.edu>
2015-08-29 15:40:41 -0500

    fixed a minor typo in the config function and corrected the script tags for angular and angularroute

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 'use strict';
 
-var app = angular.module("PollApp", ['ngRoute']);
-app.controller("PreviewController", function(){
-    this.polls = polls;
-    /**
-     * function that determines an answer choice's vote percentage
-     * @param poll - The poll within the list of polls to calculate a percentage
-     *               for
-     * @param choice - The answer choice to calculate a percentage for the
-     *                 specific poll
-     */
-    this.votePercentage = function(poll, choice){
-        return (choice["votes"] / poll["votes"] * 100).toFixed(0);
-    }
-});
-
-app.config(function($routeProvider, $locationProvider){
-    $routeProvider
-    .when('/preview', {
-        template: 'preview.html',
-        controller: 'PreviewController'
+angular.module("PollApp", ['ngRoute'])
+    .controller("PreviewController", function(){
+        this.polls = polls;
+        /**
+         * function that determines an answer choice's vote percentage
+         * @param poll - The poll within the list of polls to calculate a percentage
+         *               for
+         * @param choice - The answer choice to calculate a percentage for the
+         *                 specific poll
+         */
+        this.votePercentage = function(poll, choice){
+            return (choice["votes"] / poll["votes"] * 100).toFixed(0);
+        }
     })
-    .otherwise({
-        redirectTo: '/preview'
+    .config(function($routeProvider, $locationProvider){
+        $routeProvider
+        .when('/preview', {
+            templateUrl: 'preview.html',
+            controller: 'PreviewController'
+        })
+        .otherwise({
+            redirectTo: '/preview'
+        });
+        
+        $locationProvider.html5Mode(true);
     });
-    
-    $locationProvider.html5Mode(true);
-});
