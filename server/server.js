b8b8b3f5a3b3f604438e5c567515bf02f39bbc1b
Scott Munro <scottmunro@utexas.edu>
2015-08-31 17:42:31 -0500

    creates server that listens on a specified port and returns the pollstring to the client that calls the getpolls endpoint. fixes #3 fixes #4

--- server/server.js
+++ server/server.js
@@ -1,6 +1,29 @@
-var path = process.argv[2];
-var fs = require('fs');
-
 // on startup read polls from storage and access them as JSON
+var fs = require('fs');
+var path = process.argv[2];
 var pollsString = fs.readFileSync(path, 'utf8');
 var pollsJSON = JSON.parse(pollsString);
+
+
+// setup server
+var express = require('express');
+var nodeApp = express();
+nodeApp.get('/getpolls', function(request, result) {
+	// set the response so get requests in chrome function properly
+	result.set({
+		'Access-Control-Allow-Origin': '*',
+		'Content-Type': 'application/json'
+	});
+	result.status(200);
+	result.send(pollsString);
+});
+var endpoints = ['getpolls'];
+
+var portArg = process.argv[3];
+var server = nodeApp.listen(portArg, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	endpoints.forEach(function(endpoint) {
+		console.log('Poll server listening at http://%s:%s/%s', host, port, endpoint);
+	});
+});
