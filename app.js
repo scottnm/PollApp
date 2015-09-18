9f2cf1b2e1b6c6afecaf4d4de30fbf1ed8141c0c
Scott Munro <scottmunro@utexas.edu>
2015-09-18 00:58:41 -0500

    asynchronously loads poll data into frontend

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var app = angular.module("PollApp", []);
-app.controller("PreviewController", function(){
-    this.polls = polls;
+app.controller("PreviewController", function($scope){
+    
+    $.getJSON('http://localhost:7000/getpolls', function(data){
+        $scope.$apply(function(){
+             $scope.polls = data;
+             console.log($scope.polls);
+        });
+        
+    }.bind(this));
     /**
      * function that determines an answer choice's vote percentage
      * @param poll - The poll within the list of polls to calculate a percentage
@@ -8,7 +15,7 @@ app.controller("PreviewController", function(){
      * @param choice - The answer choice to calculate a percentage for the
      *                 specific poll
      */
-    this.votePercentage = function(poll, choice){
+    $scope.votePercentage = function(poll, choice){
         return (choice["votes"] / poll["votes"] * 100).toFixed(0);
     }
 });
